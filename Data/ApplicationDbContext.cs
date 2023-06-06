a52827f8e21468c824e3fe502611e05de26296e8
Jackson Lee <jacksonlee050802@gmail.com>
2023-06-06 13:56:13 -0400

    Comment service and models

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -2,11 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TimeProject.Entities;
 
 namespace Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext:DbContext
     {
         
+
+        public DbSet<UserEntity> Users{get;set;}
+        
     }
+
+
 }
--- Services/UserServices/IUserService.cs
+++ Services/UserServices/IUserService.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Models.User;
 
 namespace Services.UserServices
 {
     public interface IUserService
     {
+        Task<bool> RegisterUserAsync(UserRegister model);
+
         
     }
 }
